7ba76025e51eab50b20ae790934401a541e1da26
pupazaclaudiu <claudiupupaza10@gmail.com>
2025-11-21 17:00:55 +0200

    templates start

--- WebSite/auth.py
+++ WebSite/auth.py
@@ -6,3 +6,11 @@ auth1 = Blueprint('auth', __name__)
 @auth1.route('/login')
 def login():
     return "<h1>Login Page</h1>"
+
+@auth1.route('/logout')
+def logout():
+    return "<h1>Logout Page</h1>"
+
+@auth1.route('/signUp')
+def sign_up():
+    return "<h1>Sign Up Page</h1>"
